c693948d8d8e465a9e36f68b34cf98b8aa2aac9b
AmavelAntonio <amavelfranciscotomas@gmail.com>
2023-06-03 12:31:21 +0100

    feat: create unique-entity-id-vo.ts

---
+++ src/shared/domain/unique-entity-id-vo.ts
@@ -0,0 +1,18 @@
+import InvalidUuidError from "../errors/invalid-uuid.error"
+import { v4 as uuidV4, validate as uuidValid} from "uuid";
+
+
+export default class UniqueEntityId {
+    constructor(public readonly id?: string) {
+        this.id = id || uuidV4();
+        this.validate()
+    }
+
+    private validate() {
+        const isValid = uuidValid(this.id);
+
+        if(!isValid) {
+            throw new InvalidUuidError()
+        }
+    }
+}
